718e2229cc53f1bcf8b7da40ab65609ed5c37207
yamanashihibiki <258513@st.takushoku-u.ac.jp>
2026-06-03 01:44:16 +0900

    1

--- kazuate.js
+++ kazuate.js
@@ -6,7 +6,7 @@ console.log('答え（デバッグ用）: ' + kotae);
 
 // 入力回数（予想回数）
 let kaisu = 0;
-
+let seikai = false;
 // そのほか，必要に応じて変数を宣言してもよい
 
 // ボタンを押した後の処理をする関数 hantei() の定義
@@ -18,37 +18,32 @@ function hantei() {
   
   // ここから: テキストボックスに指定された数値を yoso に代入する
   let yoso;
-  yoso = Number(document.querySelector("#yoso").value); 
+  yoso = Number(document.querySelector("#yoso").value);
   // ここまで: テキストボックスに指定された数値を yoso に代入する
   
   // ここから: 正解判定する
   // 　　　　  正解/不正解のときのメッセージを表示する
   let result = document.querySelector("#result");
-
-    if (owari || kaisu > 3) {
-      result.textContent =
-        "答えは " + kotae + " でした．すでにゲームは終わっています";
-      return;
-    }
-
-    if (yoso === kotae) {
-      result.textContent = "正解です．おめでとう！";
-      owari = true;
-   }
-    else if (kaisu === 3) {
-      result.textContent =
-        "まちがい．残念でした．答えは " + kotae + " です．";
-      owari = true;
-    }
-    else if (yoso < kotae) {
-      result.textContent =
-        "まちがい．答えはもっと大きいですよ";
-    }
-    else {
+  if (kaisu >= 4 || seikai) {
+    result.textContent =
+      "答えは " + kotae + " でした．すでにゲームは終わっています";
+  }
+  else if (yoso === kotae) {
+    result.textContent = "正解です．おめでとう!";
+    seikai = true;
+  }
+  else if (kaisu === 3) {
+    result.textContent =
+      "まちがい．残念でした．答えは " + kotae + " です．";
+  }
+  else if (yoso < kotae) {
+    result.textContent =
+      "まちがい．答えはもっと大きいですよ";
+  }
+  else {
     result.textContent =
       "まちがい．答えはもっと小さいですよ";
   }
-
   // ここまで: 正解判定する
 }
 
